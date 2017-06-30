9238e7f2bee96e25db10459ffb4f3bd454fa8531
Wesley Cox <cwesnow2000@yahoo.com>
2017-06-29 23:16:43 -0500

    Create Basics.php

---
+++ Basics.php
@@ -0,0 +1,39 @@
+<?PHP
+
+// Ternary - Condition ? True : False
+$number = (!empty($_GET['name'])? $_GET['name'] : 'Default: Name')
+
+// Dynamic Variables
+$who = $_GET['whatname'] . 'name';
+if(isset($$who)){
+    echo $$who;
+}else{
+    echo 'No Name';
+}
+
+// Pass by Reference
+function lowercase(&$string){
+    $string = strtolower($string);
+}
+
+$name = 'EXAMPLE';
+lowercase($name);
+echo $name; // returns example
+
+// Array
+function buddyArray(&$array){
+    $array[0] = 'buddy';
+}
+
+$friends = array(
+    'matthew',
+    'mark',
+    'luke',
+    'john'
+);
+
+buddyArray($friends);
+
+print_r($friends); // This will now have changed 'john' to 'potato'
+?>
+
