c9595b144bc2b585bad68bef74229a05ed5b99d6
Wesley Cox <cwesnow2000@yahoo.com>
2017-07-02 00:44:02 -0500

    Update Print.php

--- Basics/Print.php
+++ Basics/Print.php
@@ -1,4 +1,8 @@
 <? PHP
 echo 'Writing a sentence is hard work!';
-echo "Writing another sentence is \"hard\" work!";
+echo "Writing a sentence that uses \"Escape Characters\" is hard work!";
+
+// Printing a Variable
+$subject = 'math';
+echo "I really enjoyed $subject this year."
 ?>
